53ae04db61ee2ffd32dbe79435e735919f30468a
heqm <messikiller@aliyun.com>
2019-04-10 15:08:15 +0800

    fuck react router v4

--- resources/assets/js/components/front/App.js
+++ resources/assets/js/components/front/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { TabBar } from 'antd-mobile';
+import { HashRouter, Route } from "react-router-dom";
 import User from './views/User';
 import ThroughBus from './views/ThroughBus';
 
@@ -8,50 +9,62 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: 'user',
-      hidden: false
+      selectedTab: 'user'
     };
+  }
+
+  componentWillMount() {
+    const hash = window.location.hash
+    const selectedTab = hash.substr(2, hash.length) || 'user'
+    this.setState({
+      selectedTab: selectedTab
+    })
   }
 
   render() {
     return (
-      <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
-        <TabBar
-          unselectedTintColor="#949494"
-          tintColor="#33A3F4"
-          barTintColor="white"
-          hidden={this.state.hidden}
-        >
-          <TabBar.Item
-            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
-            title="景区直通车"
-            key="through_bus"
-            selected={this.state.selectedTab === 'through_bus'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'through_bus',
-              });
-            }}
+      <HashRouter>
+        <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
           >
-            <ThroughBus />
-          </TabBar.Item>
-          <TabBar.Item
-            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
-            title="个人中心"
-            key="user"
-            selected={this.state.selectedTab === 'user'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'user',
-              });
-            }}
-          >
-            <User />
-          </TabBar.Item>
-        </TabBar>
-      </div>
+            <TabBar.Item
+              icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+              selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+              title="景区直通车"
+              key="through_bus"
+              selected={this.state.selectedTab === 'through_bus'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'through_bus'
+                }, () => {
+                  window.location = ('#/through_bus')
+                });
+              }}
+            >
+              <Route path="/through_bus" component={ThroughBus} />
+            </TabBar.Item>
+            <TabBar.Item
+              icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+              selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+              title="个人中心"
+              key="user"
+              selected={this.state.selectedTab === 'user'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'user'
+                }, () => {
+                  window.location = ('#/user')
+                });
+              }}
+            >
+              <Route path="/user" component={User} />
+            </TabBar.Item>
+          </TabBar>
+        </div>
+      </HashRouter>
     );
   }
 }
--- resources/assets/js/components/front/views/ThroughBus.js
+++ resources/assets/js/components/front/views/ThroughBus.js
@@ -8,10 +8,10 @@ export default class ThroughBus extends React.Component {
     super(props);
     this.state = {
       list: [
-        { src: '火车站', dest: '花溪', price: '123', status: '0', left_at: '12:00', 'arrived_at': '22: 15' },
-        { src: '大学城', dest: '贵阳大学', price: '57', status: '1', left_at: '14:00', 'arrived_at': '21: 30' },
-        { src: '花溪', dest: '风景区', price: '28', status: '0', left_at: '8:20', 'arrived_at': '20: 00' },
-        { src: '贵州大学', dest: '杜鹃风景区', price: '65', status: '0', left_at: '13:50', 'arrived_at': '17: 30' }
+        { id: 1, src: '火车站', dest: '花溪', price: '123', status: '0', left_at: '12:00', 'arrived_at': '22: 15' },
+        { id: 2, src: '大学城', dest: '贵阳大学', price: '57', status: '1', left_at: '14:00', 'arrived_at': '21: 30' },
+        { id: 3, src: '花溪', dest: '风景区', price: '28', status: '0', left_at: '8:20', 'arrived_at': '20: 00' },
+        { id: 4, src: '贵州大学', dest: '杜鹃风景区', price: '65', status: '0', left_at: '13:50', 'arrived_at': '17: 30' }
       ]
     }
   }
@@ -29,7 +29,12 @@ export default class ThroughBus extends React.Component {
                   <div>终点：{item.dest}（{item.arrived_at}）</div>
                 </div>
                 <div className="flex-extra">
-                  <Button type={item.status == 1 ? 'default' : 'ghost'} size="small" inline disabled={item.status == 1}>{item.status == 1 ? '已售罄' : `价格：${item.price}￥/人`}</Button>
+                  <Button
+                    type={item.status == 1 ? 'default' : 'ghost'}
+                    size="small"
+                    inline
+                    disabled={item.status == 1}
+                  >{item.status == 1 ? '已售罄' : `${item.price}￥/人购买`}</Button>
                 </div>
               </div>
             </Card.Body>
